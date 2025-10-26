298ac2bbe5edfe6ca9f8439dc4256f9714a08e62
unknown <filipbukowiec92@gmail.com>
2025-10-26 22:17:12 +0100

    add bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { provideHttpClient } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 export const appConfig = {
   providers: [
     provideHttpClient(),
-  ]
+    NgbModule,
+    BrowserAnimationsModule]
 };
 
 
